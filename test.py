208825980db8699ae939757a3cd1b931461f812c
turtwig12 <thomp.ed@outlook.com>
2025-06-18 14:34:10 +0100

    Add files via upload
    updated test.py still needs to be added into the main script still

--- test.py
+++ test.py
@@ -1,46 +1,60 @@
 from PIL import Image
 
-# Load the base image and the overlay image
-Map_image = Image.open("World.png").convert("RGBA")
-ISS_image = Image.open("ISS.png").convert("RGBA")
+ISS_lat=0
+ISS_lng=0
+your_lat=80
+your_lng=0
 
-lat=0.0
-lng=0.0
 
-def find_iss_cords(lat, lng):
-    x = 0.0
-    y = 0.0
+def display_map(ISS_lat, ISS_lng, your_lat, your_lng):
+    Map_image = Image.open("World.png").convert("RGBA")
+    ISS_image = Image.open("ISS.png").convert("RGBA")
+    your_image = Image.open("stickman.png").convert("RGBA")
 
-    if lng <= 0:
-        x = (180 + lng)*10
+    if ISS_lng <= 0:
+        ISS_x = (180 + ISS_lng)*10
     else:
-        lng = -lng
-        x = (180 - lng)*10
+        ISS_lng = -ISS_lng
+        ISS_x = (180 - ISS_lng)*10
 
-    if lng <= 0:
-        y = (90 + lat)*10
+    if ISS_lng <= 0:
+        ISS_y = (90 + ISS_lat)*10
     else:
-        lat = -lat
-        y = (90 - lat)*10
+        ISS_lat = -ISS_lat
+        ISS_y = (90 - ISS_lat)*10
 
-    x=int(x)
-    y=int(y)
-    return x, y
+    ISS_x=int(ISS_x)
+    ISS_y=int(ISS_y)
 
-ISS_image = ISS_image.resize((100, 100))
 
-x, y = find_iss_cords(lat, lng)
+    if your_lng <= 0:
+        your_x = (180 + your_lng)*10
+    else:
+        your_lng = -your_lng
+        your_x = (180 - your_lng)*10
 
-# Define the position where the overlay image will be placed
-position = (x, y)
+    if your_lng <= 0:
+        your_y = (90 + your_lat)*10
+    else:
+        your_lat = -your_lat
+        your_y = (90 - your_lat)*10
 
-# Create a copy of the base image to avoid modifying the original
-combined_image = Map_image.copy()
+    your_x=int(your_x)
+    your_y=int(your_y)
 
-# Paste the overlay image onto the base image using its alpha channel for transparency
-combined_image.paste(ISS_image, position, ISS_image)
 
-# Save or display the result
-combined_image.save("combined_image.png")
-combined_image.show()
+    ISS_image = ISS_image.resize((100, 100))
+    your_image = your_image.resize((50, 50))
+
+    combined_image = Map_image.copy()
+
+    combined_image.paste(ISS_image, (ISS_x, ISS_y), ISS_image)
+
+    combined_image.paste(your_image, (your_x, your_y), your_image)
+
+    combined_image.show()
+
+display_map(ISS_lat, ISS_lng, your_lat, your_lng)
+
+
 
